a8649541f3dc21fe365f269ac36f5191f46449d8
fran <125706199+YooPuiPui@users.noreply.github.com>
2026-03-16 14:35:21 -0300

    update

--- Codigo.js
+++ Codigo.js
@@ -4,5 +4,5 @@ let Number2 = 23;
 
 let Sum = Number1 + Number2;
 
-console.log("Si sumamos " + Number1 + " y " + Number2 + " el resultado es: " + Sum);
+console.log("Si sumamos " + Number1 + " y " + Number2 + " el resultado es: " + resta);
 
