f5b3469e5985b9ef004c189ca37473c8786fe6c1
fran <125706199+YooPuiPui@users.noreply.github.com>
2026-03-16 15:10:09 -0300

    update

--- Codigo.js
+++ Codigo.js
@@ -4,4 +4,4 @@ let Number2 = 2;
 
 let Sumatoria = Number1 + Number2;
 
-console.log("Si sumamos " + Number1 + " y " + Number2 + " el resultado es: " + Sumatoria + ".");
+console.log("El resultado de " + Number1 + " y " + Number2 + " es " + Sumatoria + ".");
