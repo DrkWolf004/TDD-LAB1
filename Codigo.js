68dceff5ca066a24200163b7119fc0278ea89bd0
Wolf <gabriel.guzman2201@alumnos.ubiobio.cl>
2026-03-16 14:29:01 -0300

    Update Codigo.js

--- Codigo.js
+++ Codigo.js
@@ -2,7 +2,7 @@
 let Number1 = 12;
 let Number2 = 23;
 
-let Sum = Number1 + Number1;
+let Sum = Number1 + Number2;
 
 console.log("Si sumamos " + Number1 + " y " + Number2 + " el resultado es: " + Sum);
 
